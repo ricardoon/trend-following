8a1a7a1133766f58a56251ea7db41a542f468ea1
Ricardo Oliveira <ron2302@gmail.com>
2022-08-17 19:31:39 -0300

    Update SaveAssetData.php

--- app/Jobs/SaveAssetData.php
+++ app/Jobs/SaveAssetData.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class SaveAssetData implements ShouldQueue
 {
@@ -41,28 +42,30 @@ class SaveAssetData implements ShouldQueue
         sleep(5);
         foreach ($this->assets as $asset) {
             $binance = new Binance(env('BINANCE_API_KEY'), env('BINANCE_API_SECRET'));
-            $candles = $binance->trade()->getCandle([
+            $candle = $binance->trade()->getCandle([
                 'symbol' => $asset->code,
                 'interval' => '1m',
                 'limit' => 2,
             ]);
 
-            foreach ($candles as $candle) {
-                try {
-                    History::create([
-                        'asset_id' => $asset->id,
-                        'date' => date('Y-m-d H:i:s', $candle[0] / 1000),
-                        'open' => $candle[1],
-                        'high' => $candle[2],
-                        'low' => $candle[3],
-                        'close' => $candle[4],
-                        'adj_close' => $candle[4],
-                        'volume' => $candle[5],
-                    ]);
-                } catch (\Exception $e) {
-                    dump($e->getMessage());
-                    continue;
-                }
+            try {
+                History::create([
+                    'asset_id' => $asset->id,
+                    'date' => date('Y-m-d H:i:s', $candle[0][0] / 1000),
+                    'open' => $candle[0][1],
+                    'high' => $candle[0][2],
+                    'low' => $candle[0][3],
+                    'close' => $candle[0][4],
+                    'adj_close' => $candle[0][4],
+                    'volume' => $candle[0][5],
+                ]);
+            } catch (\Exception $e) {
+                Log::info('Can\'t save asset candle.', [
+                    'schedule' => 'SaveAssetData',
+                    'asset' => $asset->code,
+                    'error' => $e->getMessage(),
+                ]);
+                continue;
             }
         }
     }
